0e72973b51a53a6f531ed596cb351376cae5c24f
mcalisadeghi <ali.ke970@gmail.com>
2023-09-15 18:36:12 +0330

    build shop page and cart page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/bottom_nav_bar.dart';
 import 'package:flutter_application_1/const.dart';
+import 'package:flutter_application_1/pages/cart_page.dart';
+import 'package:flutter_application_1/pages/shop_page.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,10 +12,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  void navBottomBar(int index) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (int index) => navBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
